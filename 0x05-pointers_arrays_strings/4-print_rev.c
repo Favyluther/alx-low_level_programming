54674b05b228496623ae4a7b2919f7833477fd61
favyluther <onyemkpachinecherem@gmail.com>
2022-09-19 14:25:10 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - func that prints a string, in reverse, followed by a new line
+ * @s: declaration of s and paramters for the function
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+int c;
+for (c = 0; s[c] != 0; c++)
+{
+	continue;
+}
