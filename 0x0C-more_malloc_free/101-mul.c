c21dae4e1c46cc070514aa5eb0fc74c8354bcc17
favyluther <onyemkpachinecherem@gmail.com>
2022-10-07 16:38:03 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,126 +1,156 @@
-#include "main.h"
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 
 /**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
- *
- * Return: no return.
+ * isdigits - checks if a string contains only digits
+ * @string: a string
+ * Return: 1 if string contains only nums, else 0
  */
-void _is_zero(char *argv[])
+int isdigits(const char *string)
 {
-	int i, isn1 = 1, isn2 = 1;
+	unsigned int i;
 
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
+	for (i = 0; string[i]; i++)
+		if (string[i] > '9' || string[i] < '0')
+			return (0);
+	return (1);
+}
 
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
+/**
+ * Array - creates a char array of a specified size and
+ * fills it with a constant byte
+ * @size: the size of array (in bytes)
+ * @b: a constant byte
+ * Return: created array
+ */
+char *Array(unsigned int size, char b)
+{
+	unsigned int i;
+	char *buffer;
 
-	if (isn1 == 1 || isn2 == 1)
+	buffer = (char *)malloc(size);
+	if (buffer == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		buffer[i] = b;
+	return (buffer);
+}
+
+/**
+ * toint - converts a char into it's appropriate int value
+ * @n: the char to be converted
+ * Return: an integer
+ */
+int toint(char n)
+{
+	return (((int)n) - 48);
+}
+
+/**
+ * mul - multiply integers in strings
+ * @num1: first integer
+ * @num2: second integer
+ * @result: a buffer where the result would be stored
+ * Return: nothing
+ */
+void mul(char *num1, char *num2, char *result)
+{
+	unsigned int len1, len2, i_n1, i_n2, carry, n1, n2, sum;
+	int i, j;
+
+	len1 = strlen(num1);
+	len2 = strlen(num2);
+	i_n1 = i_n2 = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		printf("0\n");
-		exit(0);
+		carry = 0;
+		n1 = toint(num1[i]);
+		i_n2 = 0;
+
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = toint(num2[j]);
+			sum = n1 * n2 + toint(result[i_n1 + i_n2]) + carry;
+			carry = sum / 10;
+			result[i_n1 + i_n2] = (char)(sum % 10) + 48;
+			i_n2++;
+		}
+		if (carry > 0)
+			result[i_n1 + i_n2] = (char)(toint(result[i_n1 + i_n2]) + carry) + 48;
+		i_n1++;
 	}
 }
 
 /**
- * _initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- *
- * Return: pointer of a char array.
+ * revrstr - reverse a string inplace
+ * @s: the string
+ * Return: nothing
  */
-char *_initialize_array(char *ar, int lar)
+void revrstr(char *s)
 {
-	int i = 0;
+	unsigned int ind, rind, l;
+	char c;
 
-	for (i = 0; i < lar; i++)
-		ar[i] = '0';
-	ar[lar] = '\0';
-	return (ar);
-}
-
-/**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
-int _checknum(char *argv[], int n)
-{
-	int ln;
-
-	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-	return (ln);
-}
-
-/**
- * main - Entry point.
- * program that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: arguments vector.
- *
- * Return: 0 - success.
- */
-int main(int argc, char *argv[])
-{
-	int ln1, ln2, lnout, add, addl, i, j, k, ca;
-	char *nout;
-
-	if (argc != 3)
-		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-	if (nout == NULL)
-		printf("Error\n"), exit(98);
-	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-	for (; k >= 0; k--, i--)
+	l = strlen(s);
+	ind = 0;
+	rind = l - 1;
+	while (ind < rind)
 	{
-		if (i < 0)
-		{
-			if (addl > 0)
-			{
-				add = (nout[k] - '0') + addl;
-				if (add > 9)
-					nout[k - 1] = (add / 10) + '0';
-				nout[k] = (add % 10) + '0';
-			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
-		}
-		if (j < 0)
-		{
-			if (nout[0] != '0')
-				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-		}
-		if (j >= 0)
-		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
-		}
+		c = s[ind];
+		s[ind] = s[rind];
+		s[rind] = c;
+		ind++;
+		rind--;
 	}
-	printf("%s\n", nout);
+}
+
+/**
+ * main - entry point
+ * @argc: number arguments passed
+ * @argv: list of arguments passed
+ *
+ * Return: Always 0
+ */
+int main(int argc, char const *argv[])
+{
+	unsigned int len1, len2;
+	int l;
+	char *num1, *num2, *result;
+
+	if (argc != 3 || !isdigits(argv[1]) || !isdigits(argv[2]))
+	{
+		puts("Error");
+		exit(98);
+	}
+	num1 = strdup(argv[1]);
+	if (num1 == NULL)
+		exit(98);
+	num2 = strdup(argv[2]);
+	if (num2 == NULL)
+	{
+		free(num1);
+		exit(98);
+	}
+	len1 = strlen(num1);
+	len2 = strlen(num2);
+	result = Array(len1 + len2, '0');
+	if (result == NULL)
+		exit(98);
+	mul(num1, num2, result);
+	l = strlen(result) - 1;
+	for (; l >= 0 && result[l] == '0'; l--)
+		result[l] = '\0';
+	if (l == -1)
+		puts("0");
+	else
+	{
+		revrstr(result);
+		puts(result);
+	}
+	free(result);
+	free(num1);
+	free(num2);
 	return (0);
 }
