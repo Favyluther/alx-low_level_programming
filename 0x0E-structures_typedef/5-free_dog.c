c24f943f628fb8dea16336a7501c95a71818970c
favyluther <onyemkpachinecherem@gmail.com>
2022-10-11 14:43:28 -0700

    a function that frees dogs.
    Prototype: void free_dog(dog_t *d);

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: a dog
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == N)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
