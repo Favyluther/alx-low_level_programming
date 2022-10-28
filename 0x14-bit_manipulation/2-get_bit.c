95b27b078ea6f71734de17c7e2e1661fc987acda
favyluther <onyemkpachinecherem@gmail.com>
2022-10-27 21:37:30 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index
- * @n: a denary number
- * @index: the index of the bit, starting from 0
- * Return: the value of the bit at index @index or
- *		-1 if an error occured
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit.
+ * @index: The index to get the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
  */
-int get_bit(ulong n, uint index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > (sizeof(ulong) * 8) - 1)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	return ((n & (1 << index)) >> index);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
